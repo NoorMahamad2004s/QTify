2df7b1adcf8f5317ba8c18568c60281cf0b2ce48
NoorMahamad <noormahamad.s21@gmail.com>
2024-04-29 22:28:28 +0530

    GridCard Commit

--- qtify/src/components/GridSection/GridSection.jsx
+++ qtify/src/components/GridSection/GridSection.jsx
@@ -6,37 +6,61 @@ import Card from '../Card/Card';
 import axios from "axios";
 
 const GridSection = () => {
-  const [albums, setAlbums] = useState([]);
-
-  useEffect(() => {
-    const fetchTopAlbums = async () => {
-      try {
-        const response = await axios.get("https://qtify-backend-labs.crio.do/albums/top");
-        setAlbums(response.data);
-      } catch (error) {
-        console.error("Error fetching top albums:", error);
-      }
-    };
-
-    fetchTopAlbums();
-  }, []);
-
-  return (
-    <div className={styles.gridSection}>
-      <div className={styles.header}>
-        <h3>Top Albums</h3>
-        <h4 className={styles.toggleText}>Collapse All</h4>
-      </div>
-
-      <div className={styles.cardsWrapper}>
-        <div className={styles.wrapper}>
-          {albums.map((album) => (
-            <Card key={album.id} data={album} />
-          ))}
+    const [topAlbums, setTopAlbums] = useState([]);
+    const [newAlbums, setNewAlbums] = useState([]);
+  
+    useEffect(() => {
+      const fetchTopAlbums = async () => {
+        try {
+          const response = await axios.get("https://qtify-backend-labs.crio.do/albums/top");
+          setTopAlbums(response.data);
+        } catch (error) {
+          console.error("Error fetching top albums:", error);
+        }
+      };
+  
+      const fetchNewAlbums = async () => {
+        try {
+          const response = await axios.get("https://qtify-backend-labs.crio.do/albums/new");
+          setNewAlbums(response.data);
+        } catch (error) {
+          console.error("Error fetching new albums:", error);
+        }
+      };
+  
+      fetchTopAlbums();
+      fetchNewAlbums();
+    }, []);
+  
+    return (
+      <div className={styles.gridSection}>
+        <div className={styles.header}>
+          <h3>Top Albums</h3>
+          <h4 className={styles.toggleText}>Collapse All</h4>
+        </div>
+  
+        <div className={styles.cardsWrapper}>
+          <div className={styles.wrapper}>
+            {topAlbums.map((album) => (
+              <Card key={album.id} data={album} />
+            ))}
+          </div>
+        </div>
+  
+        <div className={styles.header}>
+          <h3>New Albums</h3>
+          <h4 className={styles.toggleText}>Collapse All</h4>
+        </div>
+  
+        <div className={styles.cardsWrapper}>
+          <div className={styles.wrapper}>
+            {newAlbums.map((album) => (
+              <Card key={album.id} data={album} />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
-
+    );
+  };
+  
 export default GridSection;
